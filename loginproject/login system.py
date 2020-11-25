b85eca3a21a6f3badf8fc0d4d39fe94e5126f82a
racampita <56911151+racampita@users.noreply.github.com>
2020-11-25 22:44:52 +0800

    Update login system.py

--- loginproject/login system.py
+++ loginproject/login system.py
@@ -2,14 +2,7 @@ import json
 import logedinMod
 import loginMod
 import signupMod
-#when logged in, you can delete your account
 
-
-#login system
-
-#signup system
-
-#login or sign up
 com = input("Login(1) or Signup(2): ")
 if com == "1":
 	loginMod.login()
