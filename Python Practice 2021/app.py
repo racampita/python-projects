10c06c0ad8b0f201181d14fcb5c2bec819f4c33d
racampita <56911151+racampita@users.noreply.github.com>
2021-06-24 22:28:55 +0800

    Relearning Python Pt2
    + calculator is now on loop that can make multiple operations

--- Python Practice 2021/app.py
+++ Python Practice 2021/app.py
@@ -1,15 +1,34 @@
-num1 = int(input("Enter First Number: "))
-operation = input("Enter operation: ")
-num2 = int(input("Enter Second Number: "))
+num1 = int(input())
+operation = input()
+num2 = int(input())
 
 if operation == "+":
-    print(num1 + num2)
+    result = num1 + num2
 elif operation == "-":
-    print(num1 - num2)
+    result = num1 - num2
 elif operation == "*":
-    print(num1 * num2)
+    result = num1 * num2
 elif operation == "/":
-    print(num1 / num2)
+    result = num1 / num2
 else:
     print("Invalid operation")
+
+while True:
+    operation = input()
+    print(result)
+    num1 = result
+    if operation == "=":
+        break
+    num2 = int(input())
+    if operation == "+":
+        result = num1 + num2
+    elif operation == "-":
+        result = num1 - num2
+    elif operation == "*":
+        result = num1 * num2
+    elif operation == "/":
+        result = num1 / num2
+    else:
+        print("Invalid operation")
+
 
