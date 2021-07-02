852d64ccc33233271a6b4690a9d90f7f08a24f34
racampita <56911151+racampita@users.noreply.github.com>
2021-07-02 22:06:01 +0800

    Relearning Python Pt5
    + Switching Variables shortcut

---
+++ Python Practice 2021/switch.py
@@ -0,0 +1,4 @@
+first_number = 89
+second_number = 90
+first_number, second_number = second_number, first_number
+print(first_number,second_number)
---
+++ Switch Variables.py
@@ -0,0 +1,4 @@
+first_number = 78
+second_number = 89
+first_number , second_number = second_number, first_number
+print(first_number,second_number)
